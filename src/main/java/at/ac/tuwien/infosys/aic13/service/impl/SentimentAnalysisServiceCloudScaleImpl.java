78a154f27417b74719840623ad14a92357c2adb6
Roman <Roman@192.168.0.5>
2013-11-04 11:13:58 +0100

    Moved weka file dirctory to root directory Modified twitter query to retrieve only 5 twitter pages

--- src/main/java/at/ac/tuwien/infosys/aic13/service/impl/SentimentAnalysisServiceCloudScaleImpl.java
+++ src/main/java/at/ac/tuwien/infosys/aic13/service/impl/SentimentAnalysisServiceCloudScaleImpl.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import at.ac.tuwien.infosys.aic13.service.QueryService;
 import at.ac.tuwien.infosys.aic13.service.SentimentAnalysisService;
 import at.ac.tuwien.infosys.aic13.service.ServiceException;
-import java.util.Properties;
 
 public class SentimentAnalysisServiceCloudScaleImpl implements
 		SentimentAnalysisService {
