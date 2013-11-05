9d42959dc7981ce1bc299a6ec0253a83a47fd999
Georg Abenthung <g.abenthung@projecttac.com>
2013-11-05 19:26:47 +0100

    Starting jetty programatically and running with "mvn war:war exec:exec" works.

--- src/main/java/at/ac/tuwien/infosys/aic13/Main.java
+++ src/main/java/at/ac/tuwien/infosys/aic13/Main.java
@@ -1,24 +1,40 @@
 package at.ac.tuwien.infosys.aic13;
 
+import java.io.File;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.ContextHandler;
+import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.webapp.WebAppContext;
+import org.mortbay.xml.XmlConfiguration;
+import org.springframework.web.context.ContextLoaderListener;
 
 import at.ac.tuwien.infosys.aic13.cloudscale.workers.DummyWorker;
 import at.ac.tuwien.infosys.aic13.publicdto.PublicCompany;
 import at.ac.tuwien.infosys.aic13.publicdto.PublicSentimentQuery;
 import at.ac.tuwien.infosys.aic13.publicdto.PublicSentimentQueryResult;
+import at.ac.tuwien.infosys.cloudscale.annotations.CloudScaleShutdown;
 
 public class Main {
 
+	@CloudScaleShutdown
 	public static void main(String[] args){
-		PublicSentimentQuery query = new PublicSentimentQuery();
-		PublicCompany company = new PublicCompany();
-		company.setName("BMW");
-		company.setCreationDate(new Date());
-		query.setCompany(company);
-		
-		DummyWorker x = new DummyWorker();
-		PublicSentimentQueryResult result = x.doTheAnalysisStuff(query);
-		System.out.println(result.getNumberOfTweets());
-		System.out.println(result.getSentimentValue());
+
+		Server server = new Server( 8080 );
+		WebAppContext root = new WebAppContext();
+		root.setWar("../../target/G3P3-0.0.1-SNAPSHOT.war");
+		root.setContextPath("/G3P3");
+		server.setHandler(root);
+		server.setStopAtShutdown(true);
+		try {
+			server.start();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
