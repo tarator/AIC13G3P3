0f2a716f88c371b8626ff5e3f0c2c858775ee4fc
Georg Abenthung <g.abenthung@projecttac.com>
2013-11-05 19:27:12 +0100

    Organized imports of main.

--- src/main/java/at/ac/tuwien/infosys/aic13/Main.java
+++ src/main/java/at/ac/tuwien/infosys/aic13/Main.java
@@ -1,22 +1,8 @@
 package at.ac.tuwien.infosys.aic13;
 
-import java.io.File;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.collections.map.HashedMap;
 import org.mortbay.jetty.Server;
-import org.mortbay.jetty.handler.ContextHandler;
-import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.webapp.WebAppContext;
-import org.mortbay.xml.XmlConfiguration;
-import org.springframework.web.context.ContextLoaderListener;
 
-import at.ac.tuwien.infosys.aic13.cloudscale.workers.DummyWorker;
-import at.ac.tuwien.infosys.aic13.publicdto.PublicCompany;
-import at.ac.tuwien.infosys.aic13.publicdto.PublicSentimentQuery;
-import at.ac.tuwien.infosys.aic13.publicdto.PublicSentimentQueryResult;
 import at.ac.tuwien.infosys.cloudscale.annotations.CloudScaleShutdown;
 
 public class Main {
